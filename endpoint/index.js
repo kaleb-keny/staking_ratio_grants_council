c91a368cdb1cbe292aaaa557378e1ad3f9b4fec1
Jakub Mucha <jakub.mucha@icloud.com>
2022-09-08 20:01:36 +0200

    fix: data caching
    Signed-off-by: Jakub Mucha <jakub.mucha@icloud.com>

--- endpoint/index.js
+++ endpoint/index.js
@@ -1,3 +1,4 @@
+const fs = require('fs/promises');
 const cors = require('cors');
 const express = require('express');
 const app = express();
@@ -7,9 +8,10 @@ app.use(cors());
 app.set('json spaces', 2);
 
 app.get('/', async (req, res) => {
-  const data = require('./output/output.json');
-  if (data) {
-    res.json(data);
+  const rawData = await fs.readFile(`${__dirname}/output/output.json`);
+  const parsedData = JSON.parse(rawData);
+  if (parsedData) {
+    res.json(parsedData);
   } else {
     res.status(404);
     res.end();
